a26366467c27de27883922993306cdc0b75804d2
Unknown <carlowstonisso@hotmail.com>
2017-08-12 12:12:30 -0300

    Created project 1
    Creation of the first physics project.

---
+++ Projeto-1/Assets/movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	public float velocity = 1f;
+	private float positionx = -5f;
+
+
+	// Use this for initialization
+	void Start () {
+
+		//Definindo a posicao inicial do objeto
+		transform.position = new Vector3(positionx, 0, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Atualizacao da posicao em x
+		positionx = positionx + velocity*Time.deltaTime;
+		transform.position = new Vector3(positionx, 0, 0);
+
+
+	}
+}
